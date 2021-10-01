b2ff5e8bdd6fba0725e92928448e3200c39e5180
govind6498 <govindsingh6498@gmail.com>
2021-10-01 16:54:16 +0530

    Added:Employee payroll service to read and write employee payroll to a console

--- src/main/java/com/bridgelabz/fileioexample/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/fileioexample/EmployeePayrollData.java
@@ -1,5 +1,15 @@
 package com.bridgelabz.fileioexample;
 
 public class EmployeePayrollData {
-	
+	public int employeeId;
+	public String EmployeeName;
+	public double EmployeeSalary;
+	public EmployeePayrollData(int employeeId, String EmployeeName,double EmployeeSalary) {
+		this.employeeId = employeeId;
+		this.EmployeeName = EmployeeName;
+		this.EmployeeSalary = EmployeeSalary;
+	}
+	public String toString() {
+		return "Employee id: "+employeeId+", EmployeeName: "+EmployeeName+", EmployeeSalary: "+EmployeeSalary;
+	}
 }
--- src/main/java/com/bridgelabz/fileioexample/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/fileioexample/EmployeePayrollService.java
@@ -1,7 +1,42 @@
 package com.bridgelabz.fileioexample;
 
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class EmployeePayrollService {
+	public enum IOService {
+		CONSOLE_IO,FILE_IO,DB_IO,REST_IO
+	}
+	private List<EmployeePayrollData> employeePayrollList;
+
+	public EmployeePayrollService() {
+
+	}
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
+	}
 	
+	private void readEmployeePayrollData(Scanner scannerObject) {
+		
+		System.out.println("Enter Employee Id:");
+		int id = scannerObject.nextInt();
+		System.out.println("Enter Employee Name:");
+		String name= scannerObject.next();
+		System.out.println("Enter Employee Salary:");
+		double salary = scannerObject.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id,name,salary));
+
+	}
+	private void writeEmployeePayrollData() {
+		System.out.println("\n Writing Payroll Roaster to Cosole\n"+employeePayrollList);
+	}
+	public static void main(String[] args) {
+		System.out.println("-----------Welcome to Employee Payroll Services-------------");
+		ArrayList<EmployeePayrollData>employeePayrollList = new ArrayList<EmployeePayrollData>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+		Scanner scannerObject = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(scannerObject);
+		employeePayrollService.writeEmployeePayrollData();
+	}
 }
